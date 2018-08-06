1da7ef93de3ebd58af4b2f3e75fdfe0014381e3b
Moheb12 <42156105+Moheb12@users.noreply.github.com>
2018-08-06 18:13:35 -0400

    Update Instruction_Memory.v

--- Verilog processor/Instruction_Memory.v
+++ Verilog processor/Instruction_Memory.v
@@ -1,8 +1,11 @@
 module Instruction_Memory(input[31:0] PC, output reg[31:0] Instruction);
 
 reg [31:0] PC_array [0:6]; //7 instructions
+reg [6:0] i;
 
-i <= PC/4
+
+always@(*) begin
+i <= PC/4;
 
 PC_array[0]<=32'hF8400281;
 PC_array[1]<=32'h8B010022;
@@ -12,10 +15,8 @@ PC_array[4]<=32'h91002294;
 PC_array[5]<=32'hF81F4281;
 PC_array[6]<=32'h17FFFFFA;
 
-always@(*) begin
-
 Instruction <= PC_array [i];
 
 end
 
-endmodule 
+endmodule
