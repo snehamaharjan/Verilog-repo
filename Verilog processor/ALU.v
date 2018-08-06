3abc0c11972959032a163cd51c2dc5b619508f8c
Moheb12 <42156105+Moheb12@users.noreply.github.com>
2018-08-06 17:30:24 -0400

    Update ALU.v

--- Verilog processor/ALU.v
+++ Verilog processor/ALU.v
@@ -17,74 +17,74 @@ case(ALU_control)
 
 4'b0010: // common procedure for ADD, LDUR, STUR
 begin
-ALU_Result = Read_data1 + Decoder_Mux_output;
-Zero = 1'b0;
+ALU_Result <= Read_data1 + Decoder_Mux_output;
+Zero <= 1'b0;
 end
 4'b1010: //SUB
 begin
-ALU_Result = Read_data1 - Decoder_Mux_output;
-Zero = 1'b0;
+ALU_Result <= Read_data1 - Decoder_Mux_output;
+Zero <= 1'b0;
 end
 4'b0110: //AND
 begin
-ALU_Result = Read_data1 & Decoder_Mux_output;
-Zero = 1'b0;
+ALU_Result <= Read_data1 & Decoder_Mux_output;
+Zero <= 1'b0;
 end
 4'b0100: //ORR- inclusive OR
 begin
-ALU_Result = Read_data1 | Decoder_Mux_output;
-Zero = 1'b0;
+ALU_Result <= Read_data1 | Decoder_Mux_output;
+Zero <= 1'b0;
 end
 4'b1001: //EOR- exclusive OR
 begin
-ALU_Result = Read_data1 ^ Decoder_Mux_output;
-Zero = 1'b0;
+ALU_Result <= Read_data1 ^ Decoder_Mux_output;
+Zero <= 1'b0;
 end
 4'b0101: //NOR
 begin
-ALU_Result = ~(Read_data1 | Decoder_Mux_output);
-Zero = 1'b0;
+ALU_Result <= ~(Read_data1 | Decoder_Mux_output);
+Zero <= 1'b0;
 end
 4'b1100: //NAND
 begin
-ALU_Result = ~(Read_data1 & Decoder_Mux_output);
-Zero = 1'b0;
+ALU_Result <= ~(Read_data1 & Decoder_Mux_output);
+Zero <= 1'b0;
 end
 4'b1101: //MOV
 begin
-ALU_Result = Decoder_Mux_output;
-Zero = 1'b0;
+ALU_Result <= Decoder_Mux_output;
+Zero <= 1'b0;
 end
 4'b0111: //CBZ- Compare and branch on zero
   if (Decoder_Mux_output == 32'b0) 
 begin
-Zero = 1'b1;
-ALU_Result = 32'b0;
+Zero <= 1'b1;
+ALU_Result <= 32'b0;
 end
 
 else 
 begin
-Zero = 1'b0;
-ALU_Result = 32'b0;
+Zero <= 1'b0;
+ALU_Result <= 32'b0;
 end
 
 4'b0001: //CBNZ- Compare and branch IF not zero
   if (Decoder_Mux_output != 32'b0) 
 begin
-Zero = 1'b1;
-ALU_Result = 32'b0;
+Zero <= 1'b1;
+ALU_Result <= 32'b0;
 end
 
 else 
 begin
-Zero = 1'b0;
-ALU_Result = 32'b0;
+Zero <= 1'b0;
+ALU_Result <= 32'b0;
 end
 
 default: 
 begin
-ALU_Result = 32'b0;
-Zero = 1'b0;
+ALU_Result <= 32'b0;
+Zero <= 1'b0;
 end
 endcase 
 end
