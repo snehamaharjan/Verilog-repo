a2dacdd48bbbd7fc1122c0d5ec48c419037b27be
Moheb12 <42156105+Moheb12@users.noreply.github.com>
2018-08-06 18:59:33 -0400

    Update PC_unit.v

--- Verilog processor/PC_unit.v
+++ Verilog processor/PC_unit.v
@@ -1,5 +1,10 @@
-module PC_unit(input[31:0] Sign_extend, input Branch, Uncondbranch, Zero, output [31:0] PC);
+module PC_unit(input[31:0] Sign_extend, input Branch, input Uncondbranch, input Zero, output reg [31:0] PC);
 
+reg [31:0] add;
+reg [31:0] offset;
+reg [31:0] result;
+reg Check_and_Branch;
+reg Select_x;
 
 initial begin
 PC <= 0;
@@ -7,15 +12,15 @@ end
 
 always@(*) begin
 
-wire add <= current_PC_output + 4;
+add <= PC + 4;
 
-wire offset <= sign_extend << 2; //LEFT SHIFT 2 OR (4 times sign-extension or offset)
+offset <= Sign_extend << 2; //LEFT SHIFT 2 OR (4 times sign-extension or offset)
 
-wire result <= PC + offset;
+result <= PC + offset;
 
-wire Check_and_Branch <= Branch & Zero;
+Check_and_Branch <= Branch & Zero;
 
-wire Select_x <= Check_and_Branch | Uncondbranch;
+Select_x <= Check_and_Branch | Uncondbranch;
 
 if (Select_x == 0) begin
 PC <= add ;
