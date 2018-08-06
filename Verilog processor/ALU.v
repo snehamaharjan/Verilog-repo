33d0615cde5ae78ca58e1b83b42dcdcd7bb9fe9b
anaisalas <42148652+anaisalas@users.noreply.github.com>
2018-08-06 18:23:05 -0400

    Update ALU.v

--- Verilog processor/ALU.v
+++ Verilog processor/ALU.v
@@ -1,62 +1,62 @@
 module ALU(input[31:0] Read_data1, input [3:0] ALU_control, input ALUSrc, input [31:0] Sign_extend, output reg[31:0] ALU_Result, output reg Zero); 
 
-reg [31:0] Decoder_Mux_output;
+reg [31:0] Mux_output;
   
 always @(*)
 begin
 
 if (ALUSrc == 0) begin
-Decoder_Mux_output <= Read_data1;
+Mux_output <= Read_data1;
 end
 
 else begin
-Decoder_Mux_output <= Sign_extend;
+Mux_output <= Sign_extend;
 end
 
 case(ALU_control)
 
 4'b0010: // common procedure for ADD, LDUR, STUR
 begin
-ALU_Result <= Read_data1 + Decoder_Mux_output;
+ALU_Result <= Read_data1 + Mux_output;
 Zero <= 1'b0;
 end
 4'b1010: //SUB
 begin
-ALU_Result <= Read_data1 - Decoder_Mux_output;
+ALU_Result <= Read_data1 - Mux_output;
 Zero <= 1'b0;
 end
 4'b0110: //AND
 begin
-ALU_Result <= Read_data1 & Decoder_Mux_output;
+ALU_Result <= Read_data1 & Mux_output;
 Zero <= 1'b0;
 end
 4'b0100: //ORR- inclusive OR
 begin
-ALU_Result <= Read_data1 | Decoder_Mux_output;
+ALU_Result <= Read_data1 | Mux_output;
 Zero <= 1'b0;
 end
 4'b1001: //EOR- exclusive OR
 begin
-ALU_Result <= Read_data1 ^ Decoder_Mux_output;
+ALU_Result <= Read_data1 ^ Mux_output;
 Zero <= 1'b0;
 end
 4'b0101: //NOR
 begin
-ALU_Result <= ~(Read_data1 | Decoder_Mux_output);
+ALU_Result <= ~(Read_data1 | Mux_output);
 Zero <= 1'b0;
 end
 4'b1100: //NAND
 begin
-ALU_Result <= ~(Read_data1 & Decoder_Mux_output);
+ALU_Result <= ~(Read_data1 & Mux_output);
 Zero <= 1'b0;
 end
 4'b1101: //MOV
 begin
-ALU_Result <= Decoder_Mux_output;
+ALU_Result <= Mux_output;
 Zero <= 1'b0;
 end
 4'b0111: //CBZ- Compare and branch on zero
-  if (Decoder_Mux_output == 32'b0) 
+  if (Mux_output == 32'b0) 
 begin
 Zero <= 1'b1;
 ALU_Result <= 32'b0;
@@ -69,7 +69,7 @@ ALU_Result <= 32'b0;
 end
 
 4'b0001: //CBNZ- Compare and branch IF not zero
-  if (Decoder_Mux_output != 32'b0) 
+  if (Mux_output != 32'b0) 
 begin
 Zero <= 1'b1;
 ALU_Result <= 32'b0;
