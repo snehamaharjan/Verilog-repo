89914f793f1784a84c6a48158939ce0a1b112fb3
anaisalas <42148652+anaisalas@users.noreply.github.com>
2018-08-06 18:01:21 -0400

    Update testbench.v

--- Verilog processor/testbench.v
+++ Verilog processor/testbench.v
@@ -19,13 +19,19 @@ wire [4:0] register1, register2, writeRegister; //Registers
 
 reg clock;
 
-//Instantiate an object from the decoder class
-decoder_m decoder(register1, register2, writeRegister, immediate, Reg2Loc,
-Uncondbranch, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite, ALUOp, instruction);
+//Instantiate an object
+pc_m PC_unit(Sign_extend, Branch, Uncondbranch, Zero,PC);
+instruction_m Instruction_Memory(PC, Instruction);
+operand_m Operand_Prep(Read_register1, Read_register2, Instruction_set3, Write_data, Instruction_set4, RegWrite,Read_data1, Read_data2);
+decoder_m Decoder_Controller(Instruction,Reg2Loc, Uncondbranch, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite,ALU_control,Read_register1, Instruction_set2, Instruction_set3, Instruction_set4,check,Sign_extend);
+multiplexer1_m Multiplexer1(Instruction_set2, Instruction_set3, Reg2Loc, Decoder_Mux_output);
+alu_m ALU(Read_data1, ALU_control, ALUSrc,Sign_extend,ALU_Result,Zero);
+datacache_m Data_Cache(data, addr, Data,writeData,readData);
+     
+
 
 initial begin
-        $monitor(
-"instruction: %b", instruction, "\tPC: %d", PC);
+        $monitor("instruction: %b", instruction, "\tPC: %d", PC);
 
 
 initial begin 
