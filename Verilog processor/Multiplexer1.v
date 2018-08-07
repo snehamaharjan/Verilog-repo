9f08d66548585aa189a18726889a439c097da398
Sneha <32168358+snehamaharjan@users.noreply.github.com>
2018-08-07 18:19:40 -0400

    Update Multiplexer1.v

--- Verilog processor/Multiplexer1.v
+++ Verilog processor/Multiplexer1.v
@@ -1,17 +1,18 @@
-module Multiplexer1(input[4:0] Instruction_set2, Instruction_set3, input Reg2Loc, output reg[4:0] Decoder_Mux_output);
+module Multiplexer1(input[4:0] Instruction_set2, Instruction_set3, input Reg2Loc, i output reg[4:0] Decoder_Mux_output);
+
 always @(*) begin
 
-if (Reg2Loc == 0) begin
-Decoder_Mux_output = Instruction_set2;
-end
+    if (Reg2Loc == 0) begin
+    Decoder_Mux_output = Instruction_set2;
+    end
 
-else if (Reg2Loc == 1) begin
-Decoder_Mux_output = Instruction_set3;
-end
+    else if (Reg2Loc == 1) begin
+    Decoder_Mux_output = Instruction_set3;
+    end
 
-else begin
-Decoder_Mux_output = 5'bxxxxx;
-end
+    else begin
+    Decoder_Mux_output = 5'bxxxxx;
+    end
 
 end
 endmodule 
