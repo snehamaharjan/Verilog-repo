4ee5d92ad4a313af5a57b1c0f786ddb72a577db8
Sneha <32168358+snehamaharjan@users.noreply.github.com>
2018-08-07 18:16:34 -0400

    Update Data_Cache.v

--- Verilog processor/Data_Cache.v
+++ Verilog processor/Data_Cache.v
@@ -3,29 +3,32 @@ output reg [31:0]data,  // Read data
 input [31:0]ALU_Result,    //ALU_Result 
 input [31:0]Read_data2,  //Write data or Read_data2
 input MemWrite,   //MemWrite     
-input MemRead    //MemRead
+input MemRead,    //MemRead
+output reg [5*8:0] cache_check
 );
 
 reg [28:0]setAddress[0:15]; //16 lines
 reg [31:0]setData[0:15];
 wire [31:0]addr = ALU_Result; 
-
+ 
 reg [6:0]i;
 wire [28:0]blockAddress = addr[31:3]; //tag and index
 wire [31:0]inputData = Read_data2; 
 wire writeData = MemWrite; 
 wire readData = MemRead;
 
-	always@(blockAddress, inputData, readData, writeData) begin : search
+	always@(blockAddress, inputData, writeData, readData) begin : search
 
-	for(i = 0; i < 16; i = i + 1) begin //how many lines there are
+	for(i = 0; i < 16; i = i++) begin //how many lines there are
 		if(blockAddress == setAddress[i]) begin
 
-			if(readData) begin
+			cache_check = "hit";
+
+			if(readData==1) begin
 				data = setData[i];
 			end //if
 
-			else if(writeData) begin
+			else if(writeData==1) begin
 
 				setData[i] = inputData;
 				data = setData[i];
@@ -38,6 +41,8 @@ wire readData = MemRead;
 
 		else begin //if miss
 
+			cache_check = "miss";
+
 			setData[i] = inputData;
 				data = setData[i];
 		end
@@ -48,7 +53,7 @@ end // always
 initial begin
 	setAddress[0] = 29'h00000005; setData[0] = 32'h00000005; 
 	setAddress[1] = 29'h00000007; setData[1] = 32'h02020202;
-	setAddress[2] = 29'h00000009; setData[2] = 32'h03030303;
+	setAddress[2] = 29'h00000000; setData[2] = 32'h00000003;
 	setAddress[3] = 29'h0000000B; setData[3] = 32'h04040404;
 	setAddress[4] = 29'h0000000D; setData[4] = 32'h05050505;
 	setAddress[5] = 29'h0000000F; setData[5] = 32'h06060606;
@@ -64,4 +69,4 @@ initial begin
 	setAddress[15] = 29'h0000020F; setData[15] = 32'h01234567;
 end
 
-endmodule //fullAssoociative
+endmodule //fullAssociative
